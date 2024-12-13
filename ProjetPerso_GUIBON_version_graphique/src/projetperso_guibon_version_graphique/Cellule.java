70084451dd1fa7d97a5bacfac55ca53250f29271
33783 <33783@10.3.61.43>
2024-12-13 12:57:13 +0100

    "Création d'un nouveau projet versionné"

---
+++ ProjetPerso_GUIBON_version_graphique/src/projetperso_guibon_version_graphique/Cellule.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetperso_guibon_version_graphique;
+
+/**
+ *
+ * @author 33783
+ */
+public class Cellule {
+    private boolean presenceBombe; 
+    private boolean devoilee; 
+    private int nbBombesAdjacentes; 
+    
+    public Cellule(boolean presenceBombe, boolean devoilee, int nbBombesAdjacentes) {
+        this.presenceBombe = presenceBombe;
+        this.devoilee = devoilee;
+        this.nbBombesAdjacentes = nbBombesAdjacentes;
+    }
+
+    public boolean getPresenceBombe() {
+        return presenceBombe;
+    }
+
+    public int getNbBombesAdjacentes() {
+        return nbBombesAdjacentes;
+    }
+
+    public void placerBombe() {
+        this.presenceBombe = true;
+    }
+
+    public void revelerCellule() {
+        this.devoilee = true;
+    }
+    public boolean getDevoilee() {
+    return devoilee;
+    }
+    public void setNbBombesAdjacentes(int nbBombesAdjacentes) {
+        this.nbBombesAdjacentes = nbBombesAdjacentes;
+    }
+    @Override
+    public String toString() {
+        if (!devoilee) {
+            return "?"; 
+        }
+        if (presenceBombe) {
+            return "B"; 
+        }
+        if (nbBombesAdjacentes > 0) {
+            return String.valueOf(nbBombesAdjacentes);
+        }
+        return " "; 
+    }
+}
+
---
+++ ProjetPerso_GUIBON_version_graphique/src/projetperso_guibon_version_graphique/Cellule_Graphique.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetperso_guibon_version_graphique;
+
+/**
+ *
+ * @author 33783
+ */
+public class Cellule_Graphique {
+    
+}
