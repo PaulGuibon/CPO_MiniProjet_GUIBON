ac625c0b96ce103e0ed219350c1851ee91839a65
33783 <33783@10.3.61.43>
2024-12-14 19:11:42 +0100

    "Création d'un nouveau projet versionné"

--- ProjetPerso_GUIBON_version_graphique/src/projetperso_guibon_version_graphique/Demineur_Guibon_Graphique.java
+++ ProjetPerso_GUIBON_version_graphique/src/projetperso_guibon_version_graphique/Demineur_Guibon_Graphique.java
@@ -1,49 +1,160 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package projetperso_guibon_version_graphique;
-import javax.swing.JButton;
-import java.awt.GridLayout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 /**
- *
  * @author 33783
  */
 public class Demineur_Guibon_Graphique extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Demineur_Guibon_Graphique
-     */
+    private final int NB_LIGNES = 10;
+    private final int NB_COLONNES = 10;
+    private final int NB_BOMBES = 15;
+    private JButton[][] boutons;
+    private boolean[][] estBombe;
+    private boolean[][] devoilee;
+    private int[][] bombesAdjacentes;
+
     public Demineur_Guibon_Graphique() {
         initComponents();
-    int nbLignes = 10;
-    int nbColonnes = 10;
-    Random rand = new Random();
-    int a = rand.nextInt(nbLignes);
-    int b = rand.nextInt(nbColonnes);
-    PanneauGrille.setLayout(new GridLayout(nbLignes, nbColonnes));
-    for (int i=0; i < nbLignes; i++) {
-        for (int j=0; j < nbColonnes; j++ ) {
-            if (a==i||b==j){  
-                JButton Bombe = new JButton();
-                PanneauGrille.add(Bombe); // ajout au Jpanel PanneauGrille
-            }
-            else{
-                JButton Libre = new JButton();
-                PanneauGrille.add(Libre); // ajout au Jpanel PanneauGrille
+        initialiserGrille();
+    }
+    
 
+    private void initialiserGrille() {
+        boutons = new JButton[NB_LIGNES][NB_COLONNES];
+        estBombe = new boolean[NB_LIGNES][NB_COLONNES];
+        devoilee = new boolean[NB_LIGNES][NB_COLONNES];
+        bombesAdjacentes = new int[NB_LIGNES][NB_COLONNES];
+
+        PanneauGrille.setLayout(new GridLayout(NB_LIGNES, NB_COLONNES));
+        Random rand = new Random();
+
+        // Placement aléatoire des bombes
+        int bombesPlacees = 0;
+        while (bombesPlacees < NB_BOMBES) {
+            int x = rand.nextInt(NB_LIGNES);
+            int y = rand.nextInt(NB_COLONNES);
+            if (!estBombe[x][y]) {
+                estBombe[x][y] = true;
+                bombesPlacees++;
+            }
+        }
+
+        // Calcul du nombre de bombes adjacentes
+        for (int i = 0; i < NB_LIGNES; i++) {
+            for (int j = 0; j < NB_COLONNES; j++) {
+                if (!estBombe[i][j]) {
+                    bombesAdjacentes[i][j] = compterBombesAdjacentes(i, j);
+                }
+            }
+        }
+
+        // Création des boutons et ajout des listeners
+        for (int i = 0; i < NB_LIGNES; i++) {
+            for (int j = 0; j < NB_COLONNES; j++) {
+                boutons[i][j] = new JButton();
+                boutons[i][j].setFont(new Font("Arial", Font.BOLD, 14));
+                int x = i;
+                int y = j;
+                boutons[i][j].addActionListener(e -> revelerCellule(x, y));
+                PanneauGrille.add(boutons[i][j]);
+            }
+        }
+        for (int i = 0; i < NB_LIGNES; i++) {
+    for (int j = 0; j < NB_COLONNES; j++) {
+        if (!estBombe[i][j]) {
+            bombesAdjacentes[i][j] = compterBombesAdjacentes(i, j);
+        }
+    }
+}
+
+    }
+    
+
+   private int compterBombesAdjacentes(int x, int y) {
+    int count = 0;
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            int nx = x + dx;
+            int ny = y + dy;
+            if (nx >= 0 && nx < NB_LIGNES && ny >= 0 && ny < NB_COLONNES && estBombe[nx][ny]) {
+                count++;
             }
         }
     }
-    
+    return count;
+}
+
+
+    private void revelerCellule(int x, int y) {
+        if (devoilee[x][y]) {
+            return;
+        }
+
+        devoilee[x][y] = true;
+        boutons[x][y].setEnabled(false);
+
+        if (estBombe[x][y]) {
+            boutons[x][y].setText("B");
+            boutons[x][y].setBackground(Color.RED);
+            afficherMessagePerdu();
+        } 
+        else {
+            int bombesAdj = bombesAdjacentes[x][y];
+        
+        if (bombesAdj == 0) {
+            for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                int nx = x + dx;
+                int ny = y + dy;
+                if (nx >= 0 && nx < NB_LIGNES && ny >= 0 && ny < NB_COLONNES) {
+                    revelerCellule(nx, ny);
+                    }
+                }   
+            }
+        }
+        else if (bombesAdj == 1){
+            boutons[x][y].setText( "1");
+        }
+        else if (bombesAdj == 2){
+            boutons[x][y].setText( "2");
+        }
+        else if (bombesAdj == 3){
+            boutons[x][y].setText( "3");
+        }
+}
+
+
+        if (verifierVictoire()) {
+            afficherMessageGagne();
+        }
     }
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
+
+    private boolean verifierVictoire() {
+        for (int i = 0; i < NB_LIGNES; i++) {
+            for (int j = 0; j < NB_COLONNES; j++) {
+                if (!estBombe[i][j] && !devoilee[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void afficherMessagePerdu() {
+        JOptionPane.showMessageDialog(this, "Vous avez perdu !", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+
+    private void afficherMessageGagne() {
+        JOptionPane.showMessageDialog(this, "Félicitations, vous avez gagné !", "Victoire", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -70,40 +181,8 @@ public class Demineur_Guibon_Graphique extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Demineur_Guibon_Graphique.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Demineur_Guibon_Graphique.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Demineur_Guibon_Graphique.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Demineur_Guibon_Graphique.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Demineur_Guibon_Graphique().setVisible(true);
-            }
-        });
+        java.awt.EventQueue.invokeLater(() -> new Demineur_Guibon_Graphique().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
